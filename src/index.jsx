7271c7cf66f031d3892f22187c5ab668bf6e9bb3
Sjaak Luthart <mail@sjaakluthart.nl>
2016-12-20 11:01:59 +0100

    :sparkles: Added basic emoji list render

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import emojione from 'emojione';
+import _ from 'underscore';
+import emojiList from './emoji';
 
-function EmojiMenu() {
-  return <h1>EmojiMenu</h1>;
+class EmojiMenu extends Component {
+  static createMarkup(emoji) {
+    return {
+      __html: emojione.toImage(emoji)
+    };
+  }
+
+  static propTypes = {
+    svgSprites: PropTypes.string.isRequired
+  }
+
+  constructor(props) {
+    super(props);
+
+    emojione.imageType = 'svg';
+    emojione.sprites = true;
+    emojione.imagePathSVGSprites = props.svgSprites;
+  }
+
+  render() {
+    const categories = _.chain(emojiList).pluck('category').uniq().value();
+
+    return (
+      <section>
+        {categories.map((category) => {
+          const categoryEmoji = _.where(emojiList, { category });
+          return (
+            <article key={category}>
+              <h1>{category}</h1>
+              <section className="emoji-container">
+                {categoryEmoji.map(emoji => (
+                  <div
+                    className={`emojione ${category}`}
+                    dangerouslySetInnerHTML={EmojiMenu.createMarkup(emoji.shortname)}
+                    key={`emoji-${emoji.shortname}`}
+                  />
+                ))}
+              </section>
+            </article>
+          );
+        })}
+      </section>
+    );
+  }
 }
 
 export default EmojiMenu;
