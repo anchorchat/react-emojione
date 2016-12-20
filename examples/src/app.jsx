40d293b46d316f05cf9f79ea03f826ac96966581
Sjaak Luthart <mail@sjaakluthart.nl>
2016-12-20 16:50:14 +0100

    :sparkles: Added sendEmoji

--- examples/src/app.jsx
+++ examples/src/app.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import emojione from 'emojione';
 import EmojiMenu from '../../dist/index';
 import './app.css';
 
-function App() {
-  return (
-    <section className="demo">
-      <h1>
-        <a
-          href="https://github.com/anchorchat/react-emojione"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React EmojiOne
-        </a>
-      </h1>
-      <EmojiMenu svgSprites="/emojione.svg" />
-    </section>
-  );
+emojione.imageType = 'svg';
+emojione.sprites = true;
+emojione.imagePathSVGSprites = '/emojione.svg';
+
+class App extends Component {
+  static createMarkup(shortname) {
+    return {
+      __html: emojione.toImage(shortname)
+    };
+  }
+
+  constructor() {
+    super();
+
+    this.state = {
+      emoji: ''
+    };
+
+    this.sendEmoji = this.sendEmoji.bind(this);
+  }
+
+  sendEmoji(emoji) {
+    this.setState({
+      emoji: emoji.shortname
+    });
+  }
+
+  render() {
+    return (
+      <section className="demo">
+        <h1>
+          <a
+            href="https://github.com/anchorchat/react-emojione"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React EmojiOne
+          </a>
+        </h1>
+        <div dangerouslySetInnerHTML={App.createMarkup(this.state.emoji)} />
+        <EmojiMenu svgSprites="/emojione.svg" sendEmoji={this.sendEmoji} />
+      </section>
+    );
+  }
 }
 
 export default App;
