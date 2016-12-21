de0eb0f2afa8f7aeb338a893ac31b1aabc1d34bd
Sjaak Luthart <mail@sjaakluthart.nl>
2016-12-21 12:39:23 +0100

    :lipstick: Changed default color

--- src/components/icons/icon-flags.jsx
+++ src/components/icons/icon-flags.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function IconFlags() {
   return (
-    <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24">
+    <svg fill="#B3B3B3" height="24" viewBox="0 0 24 24" width="24">
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M14.4 6L14 4H5v17h2v-7h5.6l.4 2h7V6z" />
     </svg>
