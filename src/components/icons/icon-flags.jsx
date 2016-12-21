c9cac286e681deb48a37fa94ce34e5f221640c63
Sjaak Luthart <mail@sjaakluthart.nl>
2016-12-21 16:23:56 +0100

    :sparkles: Added optional color to icons

--- src/components/icons/icon-flags.jsx
+++ src/components/icons/icon-flags.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-function IconFlags() {
+function IconFlags({ color }) {
+  const fill = color || '#B3B3B3';
+
   return (
-    <svg fill="#B3B3B3" height="24" viewBox="0 0 24 24" width="24">
+    <svg fill={fill} height="24" viewBox="0 0 24 24" width="24">
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M14.4 6L14 4H5v17h2v-7h5.6l.4 2h7V6z" />
     </svg>
   );
 }
+
+IconFlags.propTypes = {
+  color: PropTypes.string
+};
 
 export default IconFlags;
--- src/components/icons/icon-recent.jsx
+++ src/components/icons/icon-recent.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-function IconRecent() {
+function IconRecent({ color }) {
+  const fill = color || '#B3B3B3';
+
   return (
-    <svg fill="#B3B3B3" height="24" viewBox="0 0 24 24" width="24">
+    <svg fill={fill} height="24" viewBox="0 0 24 24" width="24">
       <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" />
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z" />
     </svg>
   );
 }
+
+IconRecent.propTypes = {
+  color: PropTypes.string
+};
 
 export default IconRecent;
