f442712063877022e4c5b30f610bfa82d05d4c33
Sjaak Luthart <mail@sjaakluthart.nl>
2016-12-21 12:16:53 +0100

    :sparkles: Added svg icons

---
+++ src/components/icon-flags.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function IconFlags() {
+  return (
+    <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24">
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M14.4 6L14 4H5v17h2v-7h5.6l.4 2h7V6z" />
+    </svg>
+  );
+}
+
+export default IconFlags;
